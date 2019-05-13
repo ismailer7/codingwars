4e2850770d2abb12414384453e082a65ca45c02c
ismail <ismailrzouki7@gmail.com>
2019-05-13 07:49:58 +0100

    add line.

--- pick-peaks/src/main/java/fpl/ma/main/KataMain.java
+++ pick-peaks/src/main/java/fpl/ma/main/KataMain.java
@@ -6,7 +6,7 @@ public class KataMain {
 
 	public static void main(String[] args) {
 		System.out.println(PickPeaks.getPeaks(new int[] {-4, 5, 6, 13, -4, 5, 2, 4, 3, 16, -3, 14, -5, 12}));
-
+		System.out.println("********");
 	}
 
 }
